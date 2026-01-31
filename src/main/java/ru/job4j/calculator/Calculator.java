d01c03ce2faf55f3faa2c8970c4bc847a600d57d
semenbulytov <yanina.bulytova@mail.ru>
2026-01-31 20:25:05 +0800

    4. Методы [#231984 #248044]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,15 +2,16 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-        public static void plus(int first, int second) {
-            System.out.println(first + second);
-        }
+    public static void plus(int first, int second) {
+        System.out.println(first + second);
+    }
 
-        public static void main(String[] args) {
-            Calculator.plus(1, 2);
-            Calculator.plus(10, 11);
-            Calculator.plus(100, 500);
-            Calculator.plus(4, 2);
-            Calculator.plus(3, 5);
-        }
+    public static void main(String[] args) {
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
+    }
+
 }
