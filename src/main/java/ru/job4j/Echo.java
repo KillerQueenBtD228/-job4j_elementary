e4631890d660b43772854caba40d79d49205b546
semenbulytov <yanina.bulytova@mail.ru>
2026-01-31 20:09:41 +0800

    3.1. Переназначение переменной [#232354 #248043]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,5 +1,16 @@
 package ru.job4j;
 
 public class Echo {
+    static byte age = 18;
+    char gender = 'М';
+    float high = 2F;
+    int bricks = 100000;
+    int bacteria = 546589;
+    long length = 782;
+    boolean job = true;
+    long  file = 13717323;
 
+    public static void main(String[] args) {
+        System.out.println(age);
+    }
 }
